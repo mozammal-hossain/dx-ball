123950603a98a58dffa5763ebf319d3ad351fec6
Mozammal Hossain <mozammal.hossain.dev@gmail.com>
2025-08-22 15:45:56 +0600

    feat: Create bat and collision

--- dx_ball/lib/src/components/ball.dart
+++ dx_ball/lib/src/components/ball.dart
@@ -1,8 +1,11 @@
-import 'package:dx_ball/src/brick_breaker.dart';
-import 'package:dx_ball/src/components/play_area.dart';
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
 import 'package:flutter/material.dart';
+
+import '../brick_breaker.dart';
+import 'bat.dart';
+import 'play_area.dart';
 
 class Ball extends CircleComponent
     with CollisionCallbacks, HasGameReference<BrickBreaker> {
@@ -41,9 +44,14 @@ class Ball extends CircleComponent
       } else if (intersectionPoints.first.x >= game.width) {
         velocity.x = -velocity.x;
       } else if (intersectionPoints.first.y >= game.height) {
-        removeFromParent();
+        add(RemoveEffect(delay: 0.35));
       }
+    } else if (other is Bat) {
+      velocity.y = -velocity.y;
+      velocity.x = velocity.x +
+          (position.x - other.position.x) / other.size.x * game.width * 0.3;
     } else {
+      // To here.
       debugPrint('collision with $other');
     }
   }
